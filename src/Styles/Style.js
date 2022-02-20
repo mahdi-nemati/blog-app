2269e7b80ad92930a4d75a7e2fcf377971c8c814
mahdi <this.nemati@gmail.com>
2022-02-20 20:33:43 +0330

    add styled component

---
+++ src/Styles/Style.js
@@ -0,0 +1,20 @@
+import styled, { css } from "styled-components";
+
+export const CenterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+export const BlogTitle = styled.p`
+  margin-bottom: 10px;
+  color: #5b21b6;
+  font-size: 18px;
+  background: #f5d0fe;
+  padding: 5px;
+  border-radius: 8px;
+`;
+export const Loading = styled.p`
+  color: #5b21b6;
+  font-size: 25px;
+`;
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -2,25 +2,34 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { getBlog } from "../Features/BlogSlice";
-
+import { CenterContainer, BlogTitle, Loading } from "../Styles/Style";
 const BlogList = () => {
   const { blog, error, loading } = useSelector((store) => store.blog);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBlog());
   }, []);
-  if (loading) return <p>loading...</p>;
-  if (error) return <p>something went wrong!</p>;
+  if (loading)
+    return (
+      <CenterContainer>
+        <Loading>Loading...</Loading>
+      </CenterContainer>
+    );
+  if (error) return (
+    <CenterContainer>
+      <Loading>Something went wrong !</Loading>
+    </CenterContainer>
+  );
   return (
     <main>
       {blog.blogs &&
         blog.blogs.map((b) => {
           return (
-            <div key={b._id}>
+            <CenterContainer key={b._id}>
               <Link to={`blog/${b._id}`}>
-                <p>{b.title}</p>
+                <BlogTitle>{b.title}</BlogTitle>
               </Link>
-            </div>
+            </CenterContainer>
           );
         })}
     </main>
